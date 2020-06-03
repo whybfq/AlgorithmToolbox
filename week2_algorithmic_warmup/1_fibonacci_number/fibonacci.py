83da3165521ffea48a29886548d23424fb4c5581
whybfq <littlesanner@gmail.com>
2020-06-03 08:54:39 +0700

    change fibonacci.py

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,34 @@
-# Uses python3
-def calc_fib(n):
-    if (n <= 1):
+import sys
+from math import sqrt
+
+
+def calc_fib_fast(n):
+    if n <= 1:
         return n
 
+    previous, current = 0, 1
+
+    for _ in range(n + 1):
+        previous, current = current, previous + current
+    return current
+
+
+def calc_fib(n):
+    if n <= 0:
+        return 0
+    if n <= 1:
+        return n
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+
+def fibonacci_(n):
+    a = sqrt(5)
+    b = 1 / a
+    return int(b * ((0.5 + a/2) ** n - (0.5 - a/2) ** n))
+
+
+if __name__ == '__main__':
+    n = int(sys.stdin.read())  # n = int(input())
+    print(calc_fib_fast(n))
+    print(fibonacci_(n))
+    print(calc_fib(n))
