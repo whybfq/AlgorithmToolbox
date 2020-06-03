ed37f1645dd52e558f97de48bf7ea867ac3100c3
whybfq <littlesanner@gmail.com>
2020-06-03 09:34:01 +0700

    add stressTest to fibonacci.py

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,4 +1,6 @@
 import sys
+import random
+import numpy as np
 from math import sqrt
 
 
@@ -8,27 +10,41 @@ def calc_fib_fast(n):
 
     previous, current = 0, 1
 
-    for _ in range(n + 1):
+    for _ in range(n - 1):  # for _ in range(1, n)
         previous, current = current, previous + current
     return current
 
 
 def calc_fib(n):
-    if n <= 0:
-        return 0
-    if n <= 1:
-        return n
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    # if n <= 0:
+    #     return 0
+    return n if n <= 1 else calc_fib(n - 1) + calc_fib(n - 2)
 
 
-def fibonacci_(n):
+def fibonacci_(n):  # according to the formula
     a = sqrt(5)
     b = 1 / a
     return int(b * ((0.5 + a/2) ** n - (0.5 - a/2) ** n))
 
 
+def StreeTest(M):
+    while True:  # 2. INFINITE LOOP
+        number = random.randint(1, M)  # 3. RANDOM TEST GENERATOR
+        print(f'numbers is {number}')
+
+        result1 = calc_fib_fast(number)  # 4. > 2 WAYS
+        result2 = fibonacci_(number)
+        result3 = calc_fib(number)
+        if result1 == result2 == result3:
+            print("Ok")
+        else:
+            print("Wrong answer in : ", result1, result2, result3)
+            break
+
+
 if __name__ == '__main__':
-    n = int(sys.stdin.read())  # n = int(input())
-    print(calc_fib_fast(n))
-    print(fibonacci_(n))
-    print(calc_fib(n))
+    # n = int(sys.stdin.read())  # n = int(input())
+    # print(calc_fib_fast(n))
+    # print(fibonacci_(n))
+    # print(calc_fib(n))
+    StreeTest(10)  # if number very large, it will be very slow
