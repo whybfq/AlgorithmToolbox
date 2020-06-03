76003723b74a99b54fc3eaadb3d5d64b049e348f
whybfq <littlesanner@gmail.com>
2020-06-03 13:45:02 +0700

    still have 5,6,7 problems to solve in week 2

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,31 +1,64 @@
-# Uses python3
+'''
+Code problem: fractional knapsack
+
+The first line of the input contains the number 1≤n≤103 of items and
+the weight 0≤W≤2⋅106 of a knapsack. The next n lines define
+the cost 0≤ci≤2⋅106 and the weight 0≤ wi ≤2⋅106 of i-th item
+(n, W, ci's, wi's are integers).
+
+Output the maximal possible cost of the knapsach with
+at least four digits of precision.
+
+Sample Input:
+3 50
+60 20
+100 50
+120 30
+Sample Output:
+180.0000
+
+Memory Limit: 256 MB
+Time Limit: 5 seconds
+'''
 import sys
 
 
 def get_optimal_value(capacity, weights, values):
-    n = len(values)
-    if n == 0 or capacity == 0:
-        return 0
-    value = 0.0
-    unit = []
-    for i in values:
-        for j in weights:
-            unit.append(i/j)
-    b = unit.copy()
-    b.sort(reverse=True)
+    # n = len(values)
+    #
+    # K = [[0 for x in range(capacity+1)] for x in range(n+1)]
+    #
+    # for i in range(n+1):
+    #     for w in range(capacity+1):
+    #         if i == 0 or w == 0:
+    #             K[i][w] = 0
+    #         elif weights[i-1] <= w:
+    #             K[i][w] = max(values[i-1] + K[i-1][w-weights[i-1]], K[i-1][w])
+    #         else:
+    #             K[i][w] = K[i-1][w]
+    # return K[n][capacity]
 
-    while capacity > 0:
-        capacity -= weights[unit.index(b[0])]
+    totalCost, costWeight, n = 0.0, [], len(weights)
 
-    # return value
-    pass
+    for i in range(n):
+        costWeight.append((values[i], weights[i],values[i] * 1.0/weights[i]))
+
+    sortedWeight = sorted(costWeight, key=lambda x: x[2], reverse=True)
+
+    for i in sortedWeight:
+        if capacity >= i[1]:
+            capacity -= i[1]
+            totalCost += i[0]
+        else:
+            totalCost += capacity * 1.0 / i[1] * i[0]
+            break
+    return totalCost
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    data = list(map(int, sys.stdin.read().split()))  # data = list(map(int, input().split())
     n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
-    print(values, weights)
-    # opt_value = get_optimal_value(capacity, weights, values)
-    # print("{:.10f}".format(opt_value))  # at least 4 digits
+    values = data[2:(2 * n + 2):2]  # values list
+    weights = data[3:(2 * n + 2):2]  # weights list
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))  # at least 4 digits
