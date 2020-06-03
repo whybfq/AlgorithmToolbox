cf65e04ad5ff87e0e4627212f9f251d942474d0f
whybfq <littlesanner@gmail.com>
2020-06-03 12:18:06 +0700

    still have 5,6,7 problems to solve in week 2

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,20 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m
+
+def get_change(money):
+    return int(money/10) + int(money % 10 / 5) + money % 5
+    # numCoinns = 0
+    # while money > 0:
+    #     if money > 10:
+    #         money -= 10
+    #     elif money > 5:
+    #         money -= 5
+    #     else:
+    #         money -= 1
+    #     numCoinns += 1
+    # return numCoinns
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,11 +1,24 @@
 # Uses python3
 import sys
 
-def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
 
-    return value
+def get_optimal_value(capacity, weights, values):
+    n = len(values)
+    if n == 0 or capacity == 0:
+        return 0
+    value = 0.0
+    unit = []
+    for i in values:
+        for j in weights:
+            unit.append(i/j)
+    b = unit.copy()
+    b.sort(reverse=True)
+
+    while capacity > 0:
+        capacity -= weights[unit.index(b[0])]
+
+    # return value
+    pass
 
 
 if __name__ == "__main__":
@@ -13,5 +26,6 @@ if __name__ == "__main__":
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
-    opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print(values, weights)
+    # opt_value = get_optimal_value(capacity, weights, values)
+    # print("{:.10f}".format(opt_value))  # at least 4 digits
