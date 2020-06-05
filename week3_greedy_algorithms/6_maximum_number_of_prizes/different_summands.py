a81226a24417b15817b2049474bd66c2bf3b01fd
whybfq <littlesanner@gmail.com>
2020-06-05 11:28:21 +0700

    covering_segments.py

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,4 +1,42 @@
 """
+Advanced code problem: pairwise different summands
+
+Given an integer 1≤n≤109 find the maximal number k such that n can be represented as a sum of pairwise different positive integers. In the first line output k, in the next line output k summands.
+
+Sample Input 1:
+4
+Sample Output 1:
+2
+1 3
+
+Sample Input 2:
+6
+Sample Output 2:
+3
+1 2 3
+
+Memory Limit: 256 MB
+Time Limit: 5 seconds
+'''
+n = int(input())
+
+total = 0
+increment = 1
+count = 0
+listOfNum = []
+
+while total + increment <= n:
+    total += increment
+    listOfNum.append(increment)
+    increment += 1
+    count += 1
+
+listOfNum[count-1] += n - total
+
+print(count)
+print(*listOfNum)
+
+
 Input format. Integer n.
 Output format. In the first line, output the maximum number k such that
 n can be represented as the sum of k pairwise distinct positive integers.
@@ -25,20 +63,30 @@ Input:
 Output:
 1
 2
+
+Memory Limit: 256 MB
+Time Limit: 5 seconds
+
 """
 
 import sys
 
 
 def optimal_summands(n):
-    summands = [ ]
-    # write your code here
-    return summands
+    total, increment, count = 0, 1, 0
+    listOfNum = []
+
+    while total + increment <= n:
+        total += increment
+        listOfNum.append(increment)
+        increment += 1
+        count += 1
+    listOfNum[count - 1] += n - total
+    return listOfNum
 
 
 if __name__ == '__main__':
-    n = int(sys.stdin.read())
-    summands = optimal_summands(n)
+    summands = optimal_summands(int(sys.stdin.read()))
     print(len(summands))
     for x in summands:
         print(x, end=' ')
