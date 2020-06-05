30cfb34001c387005396872b75f82c2ba1bcd9c0
whybfq <littlesanner@gmail.com>
2020-06-05 16:34:04 +0700

    largest_number.py

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,17 +1,74 @@
-#Uses python3
+# Page 77
+
+"""
+A sample greedy algorithm
+LargestConcatenate(Numbers):
+yourSalary ← empty string
+while Numbers is not empty:
+    maxNumber ← -∞
+    for each number in Numbers:
+        if number ≥ maxNumber:
+            maxNumber ← number
+    append maxNumber to yourSalary
+    remove maxNumber from Numbers
+return yourSalary
+
+Unfortunately, this algorithm does not always maximize your salary! For example,
+for an input consisting of two integers 23 and 3 it returns 233, while the largest number is 323.
+while the algorithm works correctly for the case of single-digit numbers.
+
+improve:
+change if number ≥ maxNumber: to if IsBetter(number, maxNumber):
+
+Input format. The first line of the input contains an integer n. The second line contains integers a1, . . . , an.
+Output format. The largest number that can be composed out of a1, . . . , an. Constraints. 1 ≤ n ≤ 100; 1 ≤ ai ≤ 103 for all 1 ≤ i ≤ n. Sample 1.
+Input:
+2
+21 2
+Output:
+221
+Note that in this case the above algorithm also returns an incorrect answer 212.
+Sample 2.
+Input:
+5
+9 4 6 1 9
+Output:
+99641
+The input consists of single-digit numbers only, so the algorithm above returns the correct answer.
+Sample 3.
+Input:
+3
+23 39 92
+Output:
+923923
+The (incorrect) LargestNumber algorithm nevertheless produces the correct answer in this case,
+another reminder to always prove the correctness of your greedy algorithms!
+
+"""
 
 import sys
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+
+def IsBetter(digit, max_digit):
+    return int(str(digit) + str(max_digit)) >= int(str(max_digit) + str(digit))
+
+
+def largest_number(a: list):  # a is the numbers
+    yourSalary = []
+    while a:  # a != []
+        maxNumber = -1
+        for x in a:
+            # if int(x) >= maxNumber:
+            if IsBetter(x, maxNumber):
+                maxNumber = x
+        yourSalary.append(maxNumber)
+        a.remove(maxNumber)
+    return yourSalary
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+    # data = sys.stdin.read().split()
+    # a = data[ 1: ]
+    n = int(input())
+    lst = [int(i) for i in input().split()]
+    print(largest_number(lst))
