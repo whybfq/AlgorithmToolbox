3e3f8c84a40bdd0efa1a617cc542c6b9a7b86c23
whybfq <littlesanner@gmail.com>
2020-06-06 14:23:18 +0700

    finish largest_number.py, week3

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,28 @@
-# Uses python3
+
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
+
+def get_majority_element(seq, l, r):
+    if l + 1 == r or l + 2 == r:
+        return seq[ l ]
+    m = (l + r) // 2
+    left = get_majority_element(seq, l, m)
+    right = get_majority_element(seq, m, r)
+
+    c1, c2 = 0, 0
+    for i in seq[ l:r ]:
+        if i == left:
+            c1 += 1
+        elif i == right:
+            c2 += 1
+    if c1 > (r - l) // 2 and left != -1:
+        return left
+    elif c2 > (r - l) // 2 and right != -1:
+        return right
+    else:
         return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    n, *a = list(map(int, sys.stdin.read().split()))
+    print(int(get_majority_element(a, 0, n) != -1))
